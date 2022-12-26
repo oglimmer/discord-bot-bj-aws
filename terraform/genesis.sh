c62f1cbc5bd7a626b2b0544668d48d928b6bfb2b
Oli Zimpasser <oglimmer@gmail.com>
2022-12-27 00:51:13 +0100

    Update genesis.sh

--- terraform/genesis.sh
+++ terraform/genesis.sh
@@ -2,9 +2,7 @@
 
 set -eu
 
-[ -n "${DISCORD_BOT_BJ_CLIENT_ID:-}" ] || { echo "Env variable DISCORD_BOT_BJ_CLIENT_ID not set"; exit 1; }
-[ -n "${DISCORD_BOT_BJ_TOKEN:-}" ] || { echo "Env variable DISCORD_BOT_BJ_TOKEN not set"; exit 1; }
-[ -n "${1:-}" ] || { echo "First parameter needs to be all, apply, plan"; exit 1; }
+[ -n "${1:-}" ] || { echo "First parameter needs to be all, apply, plan, destroy"; exit 1; }
 
 TMPDIR_BASE=$(dirname $(mktemp -u))
 
@@ -12,6 +10,9 @@ if [ "$1" = "destroy" ]; then
     terraform destroy -auto-approve -var "project_region=$(aws configure get region)"
     exit 0
 fi
+
+[ -n "${DISCORD_BOT_BJ_CLIENT_ID:-}" ] || { echo "Env variable DISCORD_BOT_BJ_CLIENT_ID not set"; exit 1; }
+[ -n "${DISCORD_BOT_BJ_TOKEN:-}" ] || { echo "Env variable DISCORD_BOT_BJ_TOKEN not set"; exit 1; }
 
 if [ "$1" = "all" ]; then
     terraform init
